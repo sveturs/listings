ff39f5f2d7f41eecf2b6edbd8a5f3cf80461ff85
Sergey Bevzenko <bevzenko.sergey@gmail.com>
2025-08-29 11:35:51 +0200

    migrations

--- backend/migrations/000264_add_missing_translations.down.sql
+++
@@ -1,5 +0,0 @@
--- Откат миграции - удаление добавленных переводов
-DELETE FROM translations 
-WHERE entity_type = 'marketplace_listing' 
-AND entity_id IN (183, 250, 251, 252, 253, 254, 255, 256, 257, 258, 259, 260, 261, 262, 263, 264, 265, 266, 267, 268)
-AND is_machine_translated = true;
